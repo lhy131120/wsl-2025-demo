e92945652ce58cececf870628d02b12c40d616b9
Ray <lhy131120@gmail.com>
2025-03-14 18:15:30 +0800

    update

---
+++ assets/js/animation - 複製.js
@@ -0,0 +1,26 @@
+// const animation = {
+//   headerButtonInit: () => {
+//     return new Promise(res => {
+//       if()
+//       setTimeout(() => {
+
+//       }, 1000)
+//     })
+//   },
+// 	animationInit: () => {
+//     return new Promise((res) => {
+//       setTimeout(() => {
+//         console.log("init finished")
+//         res();
+//       }, 1000);
+//     })
+//   },
+//   animationStart: () => {
+//     return new Promise((res) => {
+//       setTimeout(() => {
+//         console.log("start finished");
+//         res();
+//       }, 2000);
+//     })
+//   }
+// };
